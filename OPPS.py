ffccf4171ae879738402e89fe528e24961352935
Manikanta Padira <padiramanikanta02@gmail.com>
2025-12-19 16:44:02 +0530

    Add files via upload

---
+++ OPPS.py
@@ -0,0 +1,24 @@
+#oops
+class login:
+    def_init_(first,username,password):
+        first.username = username
+        first.password = password
+    def output(first):
+        print(firdt.username)
+        print(first.password)
+
+obj1 = login("","")
+obj2 = login("","")
+obj1.output
+
+#create a registration class
+class registration:
+    def _init_ (first,name,email,password):
+        first.name= name
+        first.email=email
+        first.password = password
+    def display(self):
+        print(first.name)
+        print(first.email)
+        print(first.password)
+    
